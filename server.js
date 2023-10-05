9784e446e8688779aa2041d5e6cadf84b16170ed
KalashShah19 <kalashgamer19@gmail.com>
2023-10-06 01:30:01 +0530

    home night

--- server.js
+++ server.js
@@ -31,33 +31,85 @@ app.use(express.static('videos'));
 
 
 app.get('/home', (req, res) => {
-  res.sendFile(__dirname + '/home.html'); // Replace with the actual path to your home.html file
+  res.render('home', {});
+});
+
+app.get('/logout', (req, res) => {
+  req.session.destroy();
+  res.redirect('/login');
 });
 
 // Serve the registration form
 app.get('/register', (req, res) => {
-  res.render('registration', {  });
+  res.render('registration', {});
+});
+
+app.get('/change', (req, res) => {
+  if (req.session && req.session.username) {
+    res.render('change', {});
+  }
+  else {
+    res.redirect('/login');
+  }
+});
+
+app.post('/change', async (req, res) => {
+  if (req.session && req.session.username) {
+    res.render('change', {});
+  }
+  else {
+    res.redirect('/login');
+  }
+});
+
+app.post('/save', async (req, res) => {
+  const newp = req.body.new;
+  const confirmp = req.body.confirm;
+
+  if (newp === confirmp) {
+    // Passwords match, proceed with the update
+    const uid = req.session.uid; // Assuming you have a user ID in the session
+
+    try {
+      // Update the user's password in the database
+      const connection = await pool.getConnection();
+      await connection.execute('UPDATE users SET password = ? WHERE id = ?', [newp, uid]);
+
+      res.send("<script>alert('Password Changed Successfully!'); window.location.href = '/profile'; </script>");
+
+    } catch (error) {
+      console.error('Error updating password:', error);
+    }
+  } else {
+    // Passwords do not match
+    console.log("Passwords don't match");
+  }
 });
 
 app.get('/login', (req, res) => {
-  res.render('login', { msg : 'Login to Your Account' });
+  if (req.session && req.session.username) {
+    res.redirect('/user');
+  }
+  else {
+    res.render('login', { msg: 'Login to Your Account' });
+  }
 });
 
 app.get('/newLogin', (req, res) => {
-  res.render('login', { msg : 'Registration Successfull ! Login to Your Account' });
+  res.render('login', { msg: 'Registration Successfull ! Login to Your Account' });
 });
 
 app.get('/profile', (req, res) => {
-  if (req.session && req.session.user) {
+  if (req.session && req.session.uid) {
     // The 'user' session variable is set
-    username = req.session.user.name;
-    usermobile = req.session.user.mobile;
-    useremail = req.session.user.email;
+    username = req.session.username;
+    usermobile = req.session.mobile;
+    useremail = req.session.email;
 
     res.render('profile', { name: username, email: useremail, mobile: usermobile });
   } else {
     // The 'user' session variable is not set
-    // res.send('Session is not set');
+    res.redirect('/login');
     console.log('failed');
   }
 });
@@ -85,18 +137,25 @@ app.post('/auth', async (req, res) => {
   const { email, password } = req.body;
   try {
     const user = await authenticateUser(email, password);
-    const username = user.name;
-    const id = user.id;
-    console.log(user.id);
-
+    // console.log(user);
+    
     if (user) {
+      const username = user.name;
+      const id = user.id;
+      const mobile = user.mobile;
+      const useremail = user.email;
+      const pass = user.password;
       // Authentication successful
       req.session.username = username; // Store user data in the session
-      req.session.id = id; // Store user data in the session
+      req.session.uid = id; // Store user data in the session
+      req.session.email = useremail; // Store user data in the session
+      req.session.mobile = mobile; // Store user data in the session
+      req.session.password = pass; // Store user data in the session
       console.log('Login Successful');
       res.redirect('/user');
     } else {
       // Authentication failed
+      res.send("<script>alert('Login Failed !'); window.location.href = '/login'; </script>");
       console.log('Login Failed');
     }
   } catch (error) {
@@ -122,7 +181,6 @@ app.post('/registration', async (req, res) => {
     res.redirect('/newLogin');
   } catch (error) {
     console.error('Error inserting data into the database:', error);
-    res.status(500).send('Registration failed');
   }
 });
 
@@ -130,26 +188,53 @@ app.post('/registration', async (req, res) => {
 app.get('/user', async (req, res) => {
   try {
     const un = req.session.username;
-    const uid = req.session.id;
-    res.render('user', { username : un });
+    res.render('user', { username: un });
     // Select data from the 'users' table (change the query as needed)
     const connection = await pool.getConnection();
-    const [rows] = await connection.execute('SELECT * FROM users WHERE id = 1');
-    connection.release();
+    const id = req.session.uid;
+    // console.log(id);
+    const [rows] = await connection.execute('SELECT * FROM users WHERE id = ?', [id]);
 
     if (rows.length > 0) {
 
     } else {
-      res.status(404).send('User not found');
+      console.log('User Not Found');
     }
   } catch (error) {
     console.error('Error querying MySQL:', error);
-    res.status(500).send('Internal Server Error');
   }
 });
+
+// Update User Profile
+app.post('/update', async (req, res) => {
+  try {
+    // Select data from the 'users' table (change the query as needed)
+    const connection = await pool.getConnection();
+    const uid = req.session.uid;
+    const { up_name, up_email, up_mobile } = req.body;
+    console.log(up_email, up_mobile, up_name);
+
+    const [result] = await connection.execute('UPDATE users SET name = ?, email = ?, mobile = ? WHERE id = ?', [up_name, up_email, up_mobile, uid]);
+
+    if (result.affectedRows > 0) {
+      req.session.username = up_name;
+      req.session.email = up_email;
+      req.session.mobile = up_mobile;
+      res.send("<script>alert('Profile Updated Successfully!'); window.location.href = '/profile'; </script>");
+      console.log('Profile Updated Successfully !');
+    }
+    else {
+      console.log('Error while Updating Profile.');
+    }
+
+    // console.log('update');
+  } catch (error) {
+    console.error('Error querying MySQL:', error);
+  }
+});
+
 
 // Start the server
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
-
